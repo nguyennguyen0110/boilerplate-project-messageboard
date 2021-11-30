4d8158dbd976c6cfa7a5819a4bb0bc1a4230262d
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-11-30 13:43:21 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 'use strict';
+const handler = require('./handler.js');
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .get(handler.threads_get)
+    .post(handler.threads_post)
+    .delete(handler.threads_delete)
+    .put(handler.threads_put);
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .get(handler.replies_get)
+    .post(handler.replies_post)
+    .delete(handler.replies_delete)
+    .put(handler.replies_put);
 
 };
