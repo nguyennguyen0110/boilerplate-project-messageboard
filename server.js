a36ad517ff6e511c6e45b89ac3c38ec1a38ca4e0
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-11-30 13:47:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet      = require('helmet');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -16,6 +17,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.disable("x-powered-by");
+app.use(helmet({
+  dnsPrefetchControl: {allow: false},
+  frameguard: {action: "sameorigin"},
+  referrerPolicy: {policy: ["same-origin"]}
+}));
 
 //Sample front-end
 app.route('/b/:board/')
